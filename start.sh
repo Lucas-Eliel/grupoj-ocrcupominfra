38c9b04adf3c8f835a4c89fe0a0aea7095b9672c
lucaseliel <lucaseliel.business@gmail.com>
2021-12-08 21:16:57 -0300

    Correções do ambiente de infra

--- start.sh
+++ start.sh
@@ -7,3 +7,5 @@ docker network create ocr-network
 docker compose -f ./redis/docker-compose.yml up -d
 
 docker compose -f ./dynamodb/docker-compose.yml up -d
+
+docker compose -f ./localstack/docker-compose.yml up -d
--- stop.sh
+++ stop.sh
@@ -6,4 +6,6 @@ docker compose -f ./redis/docker-compose.yml down
 
 docker compose -f ./dynamodb/docker-compose.yml down
 
+docker compose -f ./localstack/docker-compose.yml down
+
 docker network rm ocr-network
