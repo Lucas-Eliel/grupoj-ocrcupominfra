11ae698ba006b6960bbf47c29f619bd0a84863bb
lucaseliel <lucaseliel.business@gmail.com>
2021-12-14 18:52:29 -0300

    Adicionando libs na infra do projeto

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 echo Iniciando a infraestrutura do projeto ocr cupom
 
+cp -R ./localstack/lambda/libs/query/ /Users/lucaseliel/Documents/projects/grupoj-ocrcupomquery
+
+cp -R ./localstack/lambda/libs/command/ /Users/lucaseliel/Documents/projects/grupoj-ocrcupomcommand
+
 docker network create ocr-network
 
 docker compose -f ./redis/docker-compose.yml up -d
@@ -9,3 +13,5 @@ docker compose -f ./redis/docker-compose.yml up -d
 docker compose -f ./dynamodb/docker-compose.yml up -d
 
 docker compose -f ./localstack/docker-compose.yml up -d
+
+
